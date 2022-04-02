290086a6e295232df6f3a306f712c49180653b71
Devjyot Singh Sidhu <devjyotsinhsidhu@gmail.com>
2022-04-02 22:14:48 +0530

    Loans Microservices completed

---
+++ loans/src/main/java/com/devus/loans/controller/LoansController.java
@@ -0,0 +1,27 @@
+package com.devus.loans.controller;
+
+import java.util.List;
+
+import com.devus.loans.model.Customer;
+import com.devus.loans.model.Loans;
+import com.devus.loans.repository.LoansRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoansController {
+    
+    @Autowired
+	private LoansRepository loansRepository;
+
+	@PostMapping("/myLoans")
+	public List<Loans> getLoansDetails(@RequestBody Customer customer) {
+		List<Loans> loans = loansRepository.findByCustomerIdOrderByStartDtDesc(customer.getCustomerId());
+
+        return loans;
+    }
+
+}
---
+++ loans/src/main/java/com/devus/loans/repository/LoansRepository.java
@@ -0,0 +1,13 @@
+package com.devus.loans.repository;
+
+import java.util.List;
+
+import com.devus.loans.model.Loans;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LoansRepository extends CrudRepository<Loans, Long> {
+    List<Loans> findByCustomerIdOrderByStartDtDesc(int customerId);
+}
